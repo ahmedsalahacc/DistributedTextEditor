56f495f08eef061748f0975443a16e6077d97c87
Kareem00101 <89580954+Kareem00101@users.noreply.github.com>
2022-06-23 19:12:06 +0200

    Edited Home Page
    Basic structure of home page html, layout for functionality implementation.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
-import './styles/Home.css'
+import React from 'react';
+//import { useNavigate } from "react-router-dom";
+import Notebook from './Notebook';
+import './styles/Home.css'; 
+//const navigate = useNavigate();
 
 function Home() {
+
+  function goToNotebook(){
+      
+    //   const routeChange = () =>{ 
+    //   let path = `newPath`; 
+    //   navigate(path);
+    // }
+    alert("Edit Me");
+  }
+
   return (
     <html>
         <head>
@@ -9,13 +22,20 @@ function Home() {
         </head>
         <body>
           <div>
-            <h1>Home Page</h1>
-            <div>
-              
+            <div class="topnav">
+              <a href="/About">About Us</a>
             </div>
-            <a href="/About">About Us</a>
+            <div>
+              <h1>Welcome To Our Multi-Editor Software</h1>
+            </div>
+            <div class="formdiv">
+              <form>
+                <label>Enter Notebook ID</label>
+                <input type="number" id="notebookID"></input>
+                <input type="submit" value="Head To Notebook" onClick={goToNotebook}></input>
+              </form>
+            </div>
           </div>
-          
         </body>
     </html>
   )
